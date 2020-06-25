930b74b8349c65523f8e9515f7ba582597fb6888
kaneco <zegomes@gmail.com>
2020-06-25 20:45:21 +0100

    Final Version -Displays logs, updates result and final winner -Added Final result feature -Added restart game functionality -Fixed various UI issues

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,10 +1,12 @@
 var possiblePlays = ["Rock","Paper","Scissors"];
 var playerSelection; //Stores the player Selection
-let finalresult;  //Stores the winner and final result
-let addhtml = document.getElementById("game");
-let images = [ 'images/rock.jpg','images/paper.jpg','images/scissors.jpg'];
-
-
+let finalResult = document.querySelector("#finalresult");
+let logs = document.getElementById("logs");
+let images = [ 'images/Rock.jpg','images/Paper.jpg','images/Scissors.jpg'];
+let userScore = document.querySelector("#USERscore");
+let cpuScore = document.querySelector("#CPUscore");
+let userImg = document.querySelector("#userimg");
+let cpuImg = document.querySelector("#cpuimg");
 const buttons = document.querySelectorAll('button');
 
 
@@ -14,105 +16,111 @@ function computerPlay(){ // Return a random play from the possible plays for the
     return cpuPlay;
 }
 
-function simulatePlaying(playerSelection, computerSelection){
-        let userImg = document.querySelector("#userimg");
-        userImg.setAttribute('src', images[0]);
-        userImg.classList.add("playing");
-        setTimeout(function(){     
-            userImg.classList.remove("playing");
-        }, 300);
-        userImg.setAttribute('src', images[1]);
-        userImg.classList.add("playing");
-        setTimeout(function(){         
-            userImg.classList.remove("playing");
-        }, 300);
-        userImg.setAttribute('src', images[2]);
-        userImg.classList.add("playing");
+// Animates the rock/paper/scissor images simulating real life and swapping to the final chosen one
+function animateImage(playerSelection, computerSelection){
+        userImg.classList.add("wobble-ver-left");
+        cpuImg.classList.add("wobble-ver-right");
+        userImg.addEventListener("animationend", () => {
+            userImg.classList.remove("wobble-ver-left");
+            cpuImg.classList.remove("wobble-ver-right");
+            userImg.setAttribute('src', "images/"+playerSelection+".jpg");
+            cpuImg.setAttribute('src', "images/"+computerSelection+".jpg");
+          });
     }
+
+
+function updateScore(winner){ // updates the score on the page given the winner
+    if (winner === "user") {
+        let newScore = parseInt(userScore.innerText);
+        userScore.innerText = newScore + 1;
+    }
+    else if (winner === "cpu") {
+        let newScore = parseInt(cpuScore.innerText);
+        cpuScore.innerText = newScore + 1;
+    }
+}
+
+
+// Toggles the buttons to disable/enable when the game ends/restarts
+function disableButtons(){
+    document.getElementById("Rock").disabled = true;
+    document.getElementById("Paper").disabled = true;
+    document.getElementById("Scissors").disabled = true;
+}
+function enableButtons(){
+    document.getElementById("Rock").disabled = false;
+    document.getElementById("Paper").disabled = false;
+    document.getElementById("Scissors").disabled = false;
+}
 
 
 function playRound(playerSelection, computerSelection) {  // plays a round of rock-paper-scissors given 2 player inputs (Player vs Computer), returns the result
     playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1).toLowerCase(); // converts player selection to allow case sensitive words
-    let userScore = document.querySelector("#USERscore");
-    let cpuScore = document.querySelector("#CPUscore");
+    userImg.setAttribute('src', images[0]);
+    cpuImg.setAttribute('src', images[0]);
+    animateImage(playerSelection, computerSelection); // playing animation for given selection
     if (playerSelection == computerSelection) {
-
         return "It\'s a tie!";
     }
     else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        userScore.innerText= 1;
+        updateScore("user");
         return "You win! Paper beats Rock!";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-        userScore.innerText= 1;
+        updateScore("cpu");
         return "You lose! Rock beats Scissors!";
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper") {
-        userScore.innerText= 1;
+        updateScore("cpu");
         return "You lose! Paper beats Rock!";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        updateScore("user");
         return "You win! Scissors beat Paper!";
     }
     else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        updateScore("user");
         return "You win! Rock beats Scissors!";
     }
     else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        updateScore("cpu");
         return "You lose! Scissors beat Paper";
     }
 }
 
-
-function game(){
-    var i;
-    let result;
-    let playerRounds = 0;
-    let cpuRounds = 0;
-    for ( i = 0; i < 5; i++) {
-        playerSelection = window.prompt("Insert your move here:");
-        result = playRound(playerSelection, computerPlay());
-        if (result.includes("win")){  //If outputted result has "win", add a round to the player and display result
-            playerRounds++;
-            console.log(result); 
-        } 
-        else if (result.includes("lose")){    //If outputted result has "lose", add a round to the cpu and display result
-            cpuRounds++;
-            console.log(result); 
-
-        }
-        else {   // If outputter result has "tie", add no rounds, just display
-            console.log(result); 
-        }
-        addhtml.insertAdjacentHTML("beforeend", result);
-        addhtml.insertAdjacentHTML("beforeend", "<p>");
-    }
-    // Displays the winner and final result of all 5 rounds
-    if (playerRounds > cpuRounds){
-        let finalresult= "You win! " + playerRounds + " to " + cpuRounds;
-        console.log(finalresult);
-        addhtml.insertAdjacentHTML("beforeend", finalresult);
-    }
-    else if (playerRounds < cpuRounds){
-        let finalresult= "You lose! " + cpuRounds + " to " + playerRounds;
-        console.log(finalresult);
-        addhtml.insertAdjacentHTML("beforeend", finalresult);
-    }
-    else {
-        let finalresult= "No one won, It's a TIE!! " + playerRounds + " to " + cpuRounds;
-        console.log(finalresult);
-        addhtml.insertAdjacentHTML("beforeend", finalresult);
-    }
-}
-
-function changeimage(image){
-    userImg.classList.remove("playing");
-
+function restart(){ //clears all the game session variables
+    userScore.innerText = 0;
+    cpuScore.innerText = 0;
+    finalResult.innerText = "";
+    logs.innerHTML = '';
+    cpuImg.style.opacity= 1;
+    userImg.style.opacity= 1;
+    userImg.setAttribute('src', images[0]);
+    cpuImg.setAttribute('src', images[0]);
+    enableButtons();
 }
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener('click', (e) => {
-        playRound(button.id, computerPlay());
+        if (button.id=="restart"){  // reset logs and score
+            restart();
+        }
+        else  {
+            result = playRound(button.id, computerPlay()); // appends logs at the end
+            logs.insertAdjacentHTML("beforeend", result);
+            logs.insertAdjacentHTML("beforeend", "<br>");
+            if (parseInt(userScore.innerText)>=5){
+                cpuImg.style.opacity= 0.3;
+                finalResult.innerText= "You won!!!  " + userScore.innerText + " to " + cpuScore.innerText;
+                disableButtons();
+            }
+            else if (parseInt(cpuScore.innerText)>=5){
+                userImg.style.opacity= 0.3;
+                finalResult.innerText= "You lost!!!  " + userScore.innerText + " to " + cpuScore.innerText;
+                disableButtons();
+            }
+        }
     });
   });
